2666839274c29968635ff0141855c2061622374b
Maple <31100207+AthenaPanda@users.noreply.github.com>
2018-07-31 22:41:00 +0800

    Update StartTest.java
    use reflection for manipulating arrays

--- StartTest.java
+++ StartTest.java
@@ -17,6 +17,7 @@ public class StartTest {
 		
 		analyzerTest();	
 		RefleationTest(args);
+		copyOfTest();
 	}
 
 	public static void showString()
@@ -265,5 +266,34 @@ public class StartTest {
 			squares.add(i*i);
 		
 		System.out.println(new ObjectAnalyzer().toString(squares));
+	}
+	
+	public static void copyOfTest()
+	{
+		int[]  a = {1, 2, 3};
+		a = (int[]) goodCopyOf(a, 10);
+		System.out.println(Arrays.toString(a));
+		
+		String[] b = {"Tom", "Dick", "Harry"};
+		b = (String[])goodCopyOf(b, 10);
+		System.out.println(Arrays.toString(b));		
+	}
+	
+	/*
+	 * This method grows an array by allocating a new array of the same type and
+	 * copying all elments
+	 * @param a the array to grow. This can be an object array or a primitive type array
+	 * @return a larger array that contains all elments of a.
+	 */
+	public static Object goodCopyOf(Object a, int newLength)
+	{
+		Class class1 = a.getClass();
+		if(!class1.isArray()) return null;
+		Class componentType = class1.getComponentType();
+		int length = Array.getLength(a);
+		Object newArray = Array.newInstance(componentType, newLength);
+		System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
+		return newArray;
+		
 	}
 }
