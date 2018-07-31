67ea7c0271d8d1e6e67ffbe720eb9eabdee7be61
Maple <31100207+AthenaPanda@users.noreply.github.com>
2018-07-31 21:59:19 +0800

    Update StartTest.java
    add reflect code

--- StartTest.java
+++ StartTest.java
@@ -14,6 +14,9 @@ public class StartTest {
 		//arraySort();
 		CalendarTest();
 		employeeCode();
+		
+		analyzerTest();	
+		RefleationTest(args);
 	}
 
 	public static void showString()
@@ -147,5 +150,108 @@ public class StartTest {
 		
 		int n = Employee.getNextId();
 		System.out.println("The next id=" + n);
+	}
+	
+	public static void RefleationTest(String[] args)
+	{
+		String name;
+		if(args.length > 0) name = args[0];
+		else
+		{
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter class name: ");
+			name = in.next();
+		}
+		
+		try 
+		{
+			Class cl = Class.forName(name);
+			Class supercl =cl.getSuperclass();
+			String modifiers = Modifier.toString(cl.getModifiers());
+			if(modifiers.length() > 0) System.out.print(modifiers + " ");
+			System.out.println("class" + name);
+			if(supercl != null && supercl != Object.class)  System.out.print("extends" + supercl.getName());
+			
+			System.out.print("\n{\n");
+			printConstructors(cl);
+			System.out.println();
+			printMethods(cl);
+			System.out.println();
+			printFields(cl);
+			System.out.println("}");
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		//System.exit(0);
+	}
+	
+	public static  void printConstructors(Class cl)
+	{
+		Constructor[] constructors = cl.getDeclaredConstructors();
+		for(Constructor c:constructors)
+		{
+			String name = cl.getName();
+			System.out.println(" ");
+			String modifiers = Modifier.toString(cl.getModifiers());
+			if(modifiers.length() > 0) System.out.print(modifiers + " ");
+			System.out.print(name +"(");
+			
+			Class[] paramTypes = c.getParameterTypes();
+			for(int j = 0; j < paramTypes.length; j++)
+			{
+				if(j > 0) System.out.println(", ");
+				System.out.print(paramTypes[j].getName());
+			}
+			System.out.println(");");
+		}
+	}
+	
+	public static void printMethods(Class cl)
+	{
+		Method[] methods = cl.getDeclaredMethods();
+		for(Method m: methods)
+		{
+			Class retType = m.getReturnType();
+			String name =m.getName();
+			
+			System.out.print(" ");
+			String modifiers = Modifier.toString(m.getModifiers());
+			if(modifiers.length() > 0) System.out.print(modifiers + " ");
+			System.out.print(retType.getName() + " " + name + "(");
+			
+			Class[] paramTypes = m.getParameterTypes();
+			for(int j = 0; j < paramTypes.length; j++)
+			{
+				if(j > 0) System.out.print(", ");
+				System.out.print(paramTypes[j].getName());
+			}
+			System.out.println(");");
+		}
+	}
+	
+	public static void printFields(Class cl)
+	{
+		Field[] fields =cl.getDeclaredFields();
+		
+		for(Field f: fields)
+		{
+			Class type = f.getType();
+			String name = f.getName();
+			System.out.print(" ");
+			String modifiers = Modifier.toString(f.getModifiers());
+			if(modifiers.length() >0) System.out.print(modifiers + " ");
+			System.out.println(type.getName() + " "+ name + ";");
+		}
+	}
+	
+	public static void analyzerTest()
+	{
+		ArrayList<Integer> squares = new ArrayList<>();
+		for(int i = 0; i <= 5; i++) 
+			squares.add(i*i);
+		
+		System.out.println(new ObjectAnalyzer().toString(squares));
 	}
 }
