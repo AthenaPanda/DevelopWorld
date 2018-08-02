b7598ad516c4ee340417e349fd48d8006f11ae05
Maple <31100207+AthenaPanda@users.noreply.github.com>
2018-08-02 23:37:31 +0800

    Update MainFrame.java
    add to draw rectangle, ellipse, line .

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,7 @@
 package frontframe;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.*;
 
 public class MainFrame extends JFrame
 {
@@ -21,7 +22,29 @@ class MainComponent extends JComponent
 	
 	public void paintComponent(Graphics g)
 	{
-		g.drawString("Hello world", MESSAGE_X, MESSAGE_Y);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		double leftX = 100;
+		double topY = 100;
+		double width = 200;
+		double height = 150;
+		
+		Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
+		g2.draw(rect);
+		
+		Ellipse2D ellipse = new Ellipse2D.Double();
+		ellipse.setFrame(rect);
+		g2.draw(ellipse);
+		
+		g2.draw(new Line2D.Double(leftX, topY, leftX + width, topY + height));
+		
+		double centerX = rect.getCenterX();
+		double centerY = rect.getCenterY();
+		double radius = 150;
+		
+		Ellipse2D circle = new Ellipse2D.Double();
+		circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+		g2.draw(circle);
 	}
 	
 	public Dimension getPreferredSize() { return new Dimension(DEFFAULT_WIDTH, DEFAULT_HEIGHT); }
