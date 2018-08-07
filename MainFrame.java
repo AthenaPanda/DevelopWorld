25812b0ea063513c222c0c6d49c4d095ca69c3b1
Maple <31100207+AthenaPanda@users.noreply.github.com>
2018-08-07 22:51:00 +0800

    Update MainFrame.java
    add mouse event code

--- MainFrame.java
+++ MainFrame.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.font.*;
+import java.awt.event.*;
+import java.util.*;
 
 public class MainFrame extends JFrame
 {
@@ -77,9 +79,19 @@ class MainComponent extends JComponent
 	
 	private Image image;
 	
+	private static final int SIDELENGTH = 10;
+	private ArrayList<Rectangle2D> squares;
+	private Rectangle2D current;
+	
 	public MainComponent()
 	{
 		image = new ImageIcon("./flower.gif").getImage();
+		
+		squares = new ArrayList<>();
+		current = null;
+		
+		addMouseListener(new MouseHandler());
+		addMouseMotionListener(new MouseMotionHandler());
 	}
 	public void paintComponent(Graphics g)
 	{
@@ -128,10 +140,88 @@ class MainComponent extends JComponent
 		Rectangle2D rect2d = new Rectangle2D.Double(x, y, bounds.getWidth(), bounds.getHeight());
 		g2.draw(rect2d);
 		
+		for(Rectangle2D r: squares)
+			g2.draw(r);
+		
 		if(image == null) return;
 		int imageWidth = image.getWidth(this);
 		int imageHeight = image.getHeight(this);
 		g2.drawImage(image, 100, 100, imageWidth, imageHeight, null);
+	}
+	
+	/*
+	 * finds the first square containng a point.
+	 * @param a point
+	 * @return the first square the contains p
+	 */
+	public Rectangle2D find(Point2D p)
+	{
+		for(Rectangle2D r: squares)
+		{
+			if(r.contains(p)) return r;
+		}
+		return null;
+	}
+	
+	/*
+	 * Add a square to the collection.
+	 * @param p the center of the square
+	 */
+	public void add(Point2D p)
+	{
+		double x = p.getX();
+		double y = p.getY();
+		
+		current = new Rectangle2D.Double(x - SIDELENGTH/2, y - SIDELENGTH/2, SIDELENGTH, SIDELENGTH);
+		squares.add(current);
+		repaint();
+	}
+	/*
+	 * remove a sqaure from the collection
+	 * @param s the square to remove
+	 */
+	public void remove(Rectangle2D s)
+	{
+		if(s == null) return;
+		if(s == current) current = null;
+		squares.remove(s);
+		repaint();
+	}
+	
+	private class MouseHandler extends MouseAdapter
+	{
+		public void mousePressed(MouseEvent event)
+		{
+			current = find(event.getPoint());
+			if(current != null) add(event.getPoint());
+		}
+		
+		public void mouseClicked(MouseEvent event)
+		{
+			current = find(event.getPoint());
+			if(current != null && event.getClickCount() >= 2) remove(current);
+		}
+	}
+	
+	private class  MouseMotionHandler implements MouseMotionListener
+	{
+		public void mouseMoved(MouseEvent event)
+		{
+			if(find(event.getPoint()) == null) setCursor(Cursor.getDefaultCursor());
+			else setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		}
+		
+		public void mouseDragged(MouseEvent event)
+		{
+			if(current != null)
+			{
+				int x = event.getX();
+				int y = event.getY();
+				
+				current.setFrame(x- SIDELENGTH/2, y - SIDELENGTH/2, SIDELENGTH, SIDELENGTH);
+				repaint();
+			}
+		}
 	}
 	
 	public Dimension getPreferredSize() { return new Dimension(DEFFAULT_WIDTH, DEFAULT_HEIGHT); }
